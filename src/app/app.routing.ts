ed694858acf0ae1a2b1c04a64f16f258519acaec
julian <juliandanielfranco@gmail.com>
2020-11-18 10:49:38 -0500

    feat: improve some inputs

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -17,4 +17,4 @@ const appRoutes: Routes =[
 
 export const appRoutingProviders: any[] = [];
 
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' });
