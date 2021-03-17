054e9fce0b0dd1ade8c92da92015189bb8b42be5
julian <juliandanielfranco@gmail.com>
2021-03-17 13:42:34 -0500

    improve faq

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
+  public panelOpenState: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
